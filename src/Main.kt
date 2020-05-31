f364b5a02c7edde166c17c47847492a44b264f82
janewaitara <janewaitara99@gmail.com>
2020-05-31 13:34:36 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+
+fun main() {
+
+    val redHeart = StringBuilder(
+        "${27.toChar()}[31m\u2665${27.toChar()}[0m"
+    )
+
+
+    println("""
+        .------.
+        |A     |
+        |     .------.
+        |   $redHeart |K     |
+        |     |      |
+        |     |   ${'\u2663'}  | 
+        `-----|      |
+              |     K|
+              `------'""".trimIndent())
+
+
+}
