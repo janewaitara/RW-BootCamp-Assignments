15d4164a69afcf8369010fd8ae9db01eb2f0ac48
janewaitara <janewaitara99@gmail.com>
2020-05-31 17:11:43 +0300

    Created a function that gets the total and the cards in hand and print out the final output

--- src/Main.kt
+++ src/Main.kt
@@ -28,21 +28,38 @@ fun dealHand(deck: MutableList<Card>,cardNumber: Int) : MutableList<Card>{
 
     val cardList: MutableList<Card> = mutableListOf()
 
-    for(index in 1..cardNumber){
+    for(i in 1..cardNumber){
         val card = createDeck().random()
         cardList.add(card)
     }
 
     return cardList
+}
+
+fun printResults(total: Int, hand: MutableList<Card>){
+    println("Your hand was: ")
+
+  for(card in hand){
+      val (pip,suit) = card
+      println(" $pip$suit")
+  }
+    println("For a total of: $total")
+    println(if (total == 21) "You Win!" else if (total == 22 )"You Lose!" else "")
 
 }
 
+
+
 fun main() {
 
-    println(createDeck())
-    println(dealHand(createDeck().toMutableList(),3))
+    //println(createDeck())
+    val hand = dealHand(createDeck().toMutableList(),2)
+    println(hand)
+    printResults(20,hand)
 
-    val redHeart = StringBuilder(
+
+
+   /* val redHeart = StringBuilder(
         "${27.toChar()}[31m\u2665${27.toChar()}[0m"
     )
 
@@ -58,5 +75,5 @@ fun main() {
               |     K|
               `------'""".trimIndent())
 
-
+*/
 }
