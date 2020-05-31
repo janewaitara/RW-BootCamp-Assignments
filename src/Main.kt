db4a7b6d7247ccf3ea69fb8b091aca34fb21d1b8
janewaitara <janewaitara99@gmail.com>
2020-05-31 13:37:30 +0300

    Created a function that returns a Mutable Set of Cards

--- src/Main.kt
+++ src/Main.kt
@@ -4,8 +4,29 @@ data class Card (
     val pip:String,
     val suit: Char)
 
+fun createDeck(): MutableSet<Card> {
+
+    val suitChar = arrayOf('\u2663','\u2660','\u2666','\u2665')
+    val pipList = listOf("K","Q","J","10","9","8","7","6","5","4","3","2","A")
+
+    val cardSet: MutableSet<Card> = mutableSetOf()
+
+    for (suit in suitChar){
+
+        for (pip in pipList){
+            var card = Card(pip,suit)
+            cardSet.add(card)
+
+        }
+    }
+
+    return cardSet
+
+}
 
 fun main() {
+
+    println(createDeck().size)
 
     val redHeart = StringBuilder(
         "${27.toChar()}[31m\u2665${27.toChar()}[0m"
