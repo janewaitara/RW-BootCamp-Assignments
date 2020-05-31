31e73170404a807fcc2bb6dcd56a6317ba106da2
janewaitara <janewaitara99@gmail.com>
2020-05-31 17:53:07 +0300

    Created a function that calculates the total of the hand : getTotal

--- src/Main.kt
+++ src/Main.kt
@@ -29,7 +29,7 @@ fun dealHand(deck: MutableList<Card>,cardNumber: Int) : MutableList<Card>{
     val cardList: MutableList<Card> = mutableListOf()
 
     for(i in 1..cardNumber){
-        val card = createDeck().random()
+        val card = deck.random()
         cardList.add(card)
     }
 
@@ -42,38 +42,60 @@ fun printResults(total: Int, hand: MutableList<Card>){
   for(card in hand){
       val (pip,suit) = card
       println(" $pip$suit")
+
+                  /* val redHeart = StringBuilder(
+            "${27.toChar()}[31m\u2665${27.toChar()}[0m"
+            )
+
+
+            println("""
+            .------.
+            |A     |
+            |     .------.
+            |   $redHeart |K     |
+            |     |      |
+            |     |   ${'\u2663'}  |
+            `-----|      |
+                  |     K|
+                  `------'""".trimIndent())
+
+            */
+
+
   }
     println("For a total of: $total")
     println(if (total == 21) "You Win!" else if (total == 22 )"You Lose!" else "")
 
 }
 
+fun getTotal(hand: MutableList<Card>): Int{
+    var total: Int = 0
+    for (index in 0..hand.lastIndex){
+        val (pip,_)= hand[index]
+        val isInteger = pip == "10"|| pip == "9"||pip == "8"|| pip == "7"||
+                pip == "6"|| pip == "5"|| pip =="4" ||pip == "3"||pip == "2"
+
+        val isFace = pip == "K"|| pip == "Q"||pip == "J"
+        total += when {
+            isFace ->  10
+            isInteger -> pip.toInt()
+            else -> 11
+        }
+    }
+    return total
+
+}
 
 
 fun main() {
 
     //println(createDeck())
     val hand = dealHand(createDeck().toMutableList(),2)
+    val totalSum =getTotal(hand)
     println(hand)
-    printResults(20,hand)
+    printResults(total = totalSum , hand = hand )
 
 
 
-   /* val redHeart = StringBuilder(
-        "${27.toChar()}[31m\u2665${27.toChar()}[0m"
-    )
 
-
-    println("""
-        .------.
-        |A     |
-        |     .------.
-        |   $redHeart |K     |
-        |     |      |
-        |     |   ${'\u2663'}  | 
-        `-----|      |
-              |     K|
-              `------'""".trimIndent())
-
-*/
 }
