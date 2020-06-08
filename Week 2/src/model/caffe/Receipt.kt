83dea93e0b9daafd5dc14de24f9fa8c0370e5923
janewaitara <janewaitara99@gmail.com>
2020-06-09 01:19:33 +0300

    Receipt class: Added data to the constructor parameters

--- Week 2/src/model/caffe/Receipt.kt
+++ Week 2/src/model/caffe/Receipt.kt
@@ -1,5 +1,10 @@
 package model.caffe
 
 // TODO add data, such as ID, list of products, and maybe an optional set of cats adopted/sponsored!
-class Receipt {
-}
+class Receipt (
+    val id: String,
+    val customerId: String,
+    val product: List<Product>,
+    val totalPrice,
+    var catsAdopted: MutableSet<Set>
+)
