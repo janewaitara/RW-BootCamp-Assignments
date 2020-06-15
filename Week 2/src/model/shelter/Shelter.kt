39cab200f9ac4a547153eb04929e7224fdc1fa7c
janewaitara <janewaitara99@gmail.com>
2020-06-15 22:23:23 +0300

    Removed redundant folders

--- Week 2/src/model/shelter/Shelter.kt
+++
@@ -1,9 +0,0 @@
-package model.shelter
-
-// TODO add remaining data
-data class Shelter(
-    val name: String,
-    val id : String,
-    val shelterAddress: String,
-    val shelterPhone: String
-)
