834de65bc7e633e0231ecb50e9ef4ca86406a7b2
janewaitara <janewaitara99@gmail.com>
2020-06-30 00:59:34 +0300

    Added a logout menu

--- Week 5/MovieAppW5/app/src/main/java/com/janewaitara/movieapp/MainActivity.kt
+++ Week 5/MovieAppW5/app/src/main/java/com/janewaitara/movieapp/MainActivity.kt
@@ -1,27 +1,49 @@
 package com.janewaitara.movieapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var loginPrefs: MovieSharedPrefs
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //loading the nav graph
-        val navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
 
-        val myPrefs = MovieSharedPrefs(this)
+         loginPrefs = MovieSharedPrefs(this)
 
 
-        when(myPrefs.getLoginStatus()){
+        when(loginPrefs.getLoginStatus()){
             false -> navController.navigate(R.id.loginFragment)
             true -> navController.navigate(R.id.movieListFragment)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.logout_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+
+        when(item.itemId){
+            R.id.log_out -> {
+                loginPrefs.setLoginStatus(false)
+                navController.navigate(R.id.loginFragment)
+            }
+        }
+        return true
     }
 
 }
