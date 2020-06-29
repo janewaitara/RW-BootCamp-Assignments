e0d129aa77a3782774a439a82ccad517863e866f
janewaitara <janewaitara99@gmail.com>
2020-06-29 19:25:08 +0300

    Added a login fragment that navigates to the movieList Fragment when the login button is clicked

---
+++ Week 5/MovieAppW5/app/src/main/java/com/janewaitara/movieapp/LoginFragment.kt
@@ -0,0 +1,65 @@
+package com.janewaitara.movieapp
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+    private var listener: OnFragmentInteractionListener? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_login.setOnClickListener {
+            view?.let {
+                it.findNavController().navigate(R.id.action_loginFragment_to_movieListFragment)
+            }
+        }
+    }
+  
+
+
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+
+    interface OnFragmentInteractionListener {
+
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = LoginFragment()
+
+    }
+}
