0adeb04119a9eacd2ce2bc2eae6b9e8edeaceb49
janewaitara <janewaitara99@gmail.com>
2020-07-06 13:43:47 +0300

    Added a ViewModel

---
+++ Week 6/MovieAppW6/app/src/main/java/com/janewaitara/movieapp/MovieViewModel.kt
@@ -0,0 +1,45 @@
+package com.janewaitara.movieapp
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MovieViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository: RoomRepository
+
+    /**Using LiveData has several benefits:
+    We can put an observer on the data (instead of polling for changes) and only update the
+    the UI when the data actually changes.
+    Repository is completely separated from the UI through the ViewModel.
+     */
+
+    //LiveData member variable to cache the list of words.
+    val allMovies: LiveData<List<Movie>>
+
+    init {
+        repository =  RoomRepository()
+        allMovies = repository.getAllMovies()
+    }
+
+    /**
+     * Launching a new coroutine to insert the data in a non-blocking way
+     */
+    fun insert(movie: Movie) = viewModelScope.launch(Dispatchers.IO)
+    {
+        repository.addMovie(movie)
+    }
+
+    fun deleteMovie(movie: Movie){
+        viewModelScope.launch {
+            repository.deleteMovie(movie)
+        }
+    }
+
+
+
+
+}
