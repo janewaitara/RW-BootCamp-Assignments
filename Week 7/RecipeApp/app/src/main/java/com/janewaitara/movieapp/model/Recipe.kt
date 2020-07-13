c894c76cad79e9d8f4d574f5b9ec8bafa406141e
janewaitara <janewaitara99@gmail.com>
2020-07-13 19:49:04 +0300

    Added a class to hold the recipes

--- Week 7/RecipeApp/app/src/main/java/com/janewaitara/movieapp/model/Recipe.kt
+++ Week 7/RecipeApp/app/src/main/java/com/janewaitara/movieapp/model/Recipe.kt
@@ -1,2 +1,13 @@
 package com.janewaitara.movieapp.model
 
+class Recipe(
+    val id : Int,
+    val title: String,
+    val readyInMinutes: Int,
+    val image: String,
+    val summary: String,
+    val instructions: String,
+    val extendedIngredients: Array<Ingredients>
+)
+
+class Ingredients ( val originalString: String)
