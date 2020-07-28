b4fec48cff91674799db0f557ed5f848c6c70f3c
janewaitara <janewaitara99@gmail.com>
2020-07-28 16:40:58 +0300

    Added logic to test searchRecipeInfo function in the SearchRecipeInfoViewMoel

---
+++ Week 9/RecipeApp/app/src/test/java/com/janewaitara/movieapp/ui/recipes/searchRecipeInformation/SearchRecipeInformationViewModelTest.kt
@@ -0,0 +1,26 @@
+package com.janewaitara.movieapp.ui.recipes.searchRecipeInformation
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SearchRecipeInformationViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getSearchedInfoRecipeLiveData() {
+    }
+
+    @Test
+    fun searchRecipeFromApiUsingSearchParameter() {
+    }
+}
